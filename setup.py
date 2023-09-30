bac4ac1e0214d8e18d54e162db012e51f91e1993
efe <113731512+efenatuyo@users.noreply.github.com>
2023-09-30 17:17:30 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Supolo',
+    version='1.0.0',
+    description='A fast discord nuke bot based package',
+    author='Xolo',
+    url='https://github.com/efenatuyo/supolo',
+    packages=find_packages(),
+    install_requires=[
+        'aiohttp',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache License 2.0',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    license='Apache License 2.0',
+)
