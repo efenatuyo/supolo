ad57c9ec8826832229b200a5d206a079d5a3c01b
efe <113731512+efenatuyo@users.noreply.github.com>
2023-09-30 17:39:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,13 +10,5 @@ setup(
     install_requires=[
         'aiohttp',
     ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache License 2.0',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-    ],
     license='Apache License 2.0',
 )
