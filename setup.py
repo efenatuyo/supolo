ac1792ade3e588a677f32cbbe8b93ba3790e2bcc
efe <113731512+efenatuyo@users.noreply.github.com>
2023-09-30 20:26:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='supolo',
-    version='1.0.0',
+    version='1.1.0',
     description='A fast discord nuke bot based package',
     author='Xolo',
     url='https://github.com/efenatuyo/supolo',
